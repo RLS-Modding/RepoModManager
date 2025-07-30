b417314fd8b9f37b4ff4f0c91cf4d9bf73916161
TristanGaldys <123184923+TristanGaldys@users.noreply.github.com>
2025-07-30 11:10:40 -0400

    - Added MCR as dependency

--- scripts/RepoModManager/modScript.lua
+++ scripts/RepoModManager/modScript.lua
@@ -1,3 +1,19 @@
+local function checkForMod(modID)
+    local mods = core_modmanager.getMods()
+    
+    for _, mod in pairs(mods) do
+        if mod.modID == modID then
+            if not mod.active then
+                core_modmanager.activateModId(mod.modID)
+            end
+            return
+        end
+    end
+    core_repository.modSubscribe(modID)
+end
+
+checkForMod("M6CZKT7NV")
+
 setExtensionUnloadMode("requiredMods", "manual")
 extensions.unload("requiredMods")
 
